4beec2bb8bdcd2b2af5736e28a6d2de5b65631e8
Ryan Wendell <ryanjwendell@gmail.com>
2026-04-26 18:12:11 +0000

    update network settings, suda-vim config

--- home/nixvim/plugins/extras.nix
+++ home/nixvim/plugins/extras.nix
@@ -1,87 +1,92 @@
 { lib, ... }:
 {
-  plugins.snacks = {
-    enable = true;
-    autoLoad = true;
-  };
+	plugins.snacks = {
+		enable = true;
+		autoLoad = true;
+	};
 
-  plugins.noice = {
-    enable = true;
-    autoLoad = true;
-    settings = {
-      presets = {
-        bottom_search = true;
-        command_palette = true;
-        long_message_to_split = true;
-        inc_rename = false;
-        lsp_doc_border = false;
-      };
-    };
-  };
+	plugins.noice = {
+		enable = true;
+		autoLoad = true;
+		settings = {
+			presets = {
+				bottom_search = true;
+				command_palette = true;
+				long_message_to_split = true;
+				inc_rename = false;
+				lsp_doc_border = false;
+			};
+		};
+	};
 
-  plugins.conform-nvim = {
-    enable = true;
-    autoLoad = true;
-    settings = {
-      default_format_opts = {
-        timeout_ms = 3000;
-        async = false;
-        quiet = false;
-        lsp_format = "fallback";
-      };
-      format_on_save = {
-        timeout_ms = 500;
-      };
-    };
-  };
+	plugins.conform-nvim = {
+		enable = true;
+		autoLoad = true;
+		settings = {
+			default_format_opts = {
+				timeout_ms = 3000;
+				async = false;
+				quiet = false;
+				lsp_format = "fallback";
+			};
+			format_on_save = {
+				timeout_ms = 500;
+			};
+		};
+	};
 
-  plugins.lazydev = {
-    enable = true;
-  };
+	plugins.lazydev = {
+		enable = true;
+	};
 
-  plugins.todo-comments = {
-    enable = true;
-  };
+	plugins.todo-comments = {
+		enable = true;
+	};
 
-  plugins.ts-comments = {
-    enable = true;
-  };
+	plugins.ts-comments = {
+		enable = true;
+	};
 
-  plugins.schemastore = {
-    enable = true;
-  };
+	plugins.schemastore = {
+		enable = true;
+	};
 
-  plugins.vimtex = {
-    enable = true;
-  };
+	plugins.vimtex = {
+		enable = true;
+	};
 
-  plugins.yanky = {
-    enable = true;
-  };
+	plugins.yanky = {
+		enable = true;
+	};
 
-  plugins.fidget = {
-    enable = true;
-  };
+	plugins.fidget = {
+		enable = true;
+	};
 
-  plugins.friendly-snippets = {
-    enable = true;
-  };
+	plugins.friendly-snippets = {
+		enable = true;
+	};
 
-  plugins.luasnip = {
-    enable = true;
-  };
+	plugins.luasnip = {
+		enable = true;
+	};
 
-  plugins.mini-files = {
-    enable = true;
-  };
+	plugins.mini-files = {
+		enable = true;
+	};
 
-  plugins.web-devicons = {
-    enable = true;
-  };
+	plugins.web-devicons = {
+		enable = true;
+	};
 
-  plugins.mini-icons = {
-    enable = true;
-  };
+	plugins.mini-icons = {
+		enable = true;
+	};
 
-  plugins.vim.suda.enable = true;
+	plugins.vim.suda = {
+		enable = true;
+		path = "doas";
+		smart_edit = 1;
+
+	};
 }
--- modules/wireless.nix
+++ modules/wireless.nix
@@ -5,6 +5,7 @@
   };
   config = lib.mkIf config.cappuccino.enableWireless {
     networking.wireless.enable = true;
+    networking.wireless.userControlled.enable = true;
     networking.wireless.secretsFile = config.age.secrets.wifi.path;
     networking.wireless.networks."espresso".pskRaw = "ext:psk";
   };
