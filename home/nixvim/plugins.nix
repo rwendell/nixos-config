90497cd61648d522ce06d3d5254f5ea1a1610fb9
Ryan Wendell <ryanjwendell@gmail.com>
2026-04-26 03:00:37 +0000

    Add more nixvim plugins: lualine, which-key, blink-cmp, telescope

--- home/nixvim/plugins.nix
+++ home/nixvim/plugins.nix
@@ -1,11 +1,33 @@
 { lib, ... }:
 {
   plugins.treesitter.enable = true;
+
   plugins.lsp.servers = {
     nil_ls.enable = true;
     rustaceanui.enable = true;
     ts_ls.enable = true;
     gopls.enable = true;
     pyright.enable = true;
+  };
+
+  plugins.lualine = {
+    enable = true;
+    autoLoad = true;
+  };
+
+  plugins.which-key = {
+    enable = true;
+  };
+
+  plugins.blink-cmp = {
+    enable = true;
+  };
+
+  plugins.gitsigns = {
+    enable = true;
+  };
+
+  plugins.telescope = {
+    enable = true;
   };
 }
