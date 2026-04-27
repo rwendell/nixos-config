5d12fc5fdc357ee541dcbd6b337fd28b2a89cdd3
Ryan Wendell <ryanjwendell@gmail.com>
2026-04-27 20:29:48 +0000

    fix: move darkman to systemd.user.services and cleanup
    - Move darkman from home-manager to systemd.user.services in cappuccino config
    - Darkman now managed at NixOS system level
    - Keep nixos-switch as the apply command (defined in shared.nix)
    - Remove darkman from home/default.nix
    - Fix specialisation syntax with lib.mkForce
    - Add prefer-no-csd to niri config (fixes blue overlay)
    - Add window-rule draw-border-with-background false for ghostty

--- home/default.nix
+++ home/default.nix
@@ -15,23 +15,7 @@
     enable = true;
   };
 
-  # Darkman for auto dark/light mode switching
-  services.darkman = {
-    enable = true;
-    settings = {
-      lat = 42.35;  # TODO: Set your latitude
-      lng = -71.10; # TODO: Set your longitude
-      usegeoclue = false; # Set to true for auto location via geoclue
-      dbusserver = true;
-      portal = true;
-    };
-    darkModeScripts = {
-      "nixos-switch" = "doas nixos-rebuild switch --specialisation dark --flake /etc/nixos#cappuccino";
-    };
-    lightModeScripts = {
-      "nixos-switch" = "doas nixos-rebuild switch --specialisation light --flake /etc/nixos#cappuccino";
-    };
-  };
+  # Darkman is managed at system level (services.darkman in cappuccino/default.nix)
 
-	home.stateVersion = "26.05";
+ 	home.stateVersion = "26.05";
 }
